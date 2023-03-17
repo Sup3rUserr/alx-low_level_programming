8995bbe1f9e8dd4b6ad176c76e23abf56821b9d0
Yassin0xb98 <majidyassine0305@gmail.com>
2023-03-16 17:02:28 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A pogram that print a line with puts fonction
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
