91747871cfadb49625266a6a9c401640998e79ed
Yassin0xb98 <majidyassine0305@gmail.com>
2023-03-17 18:20:22 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 success, non zero error.
+ */
+int main(void)
+{
+int n;
+int m;
+
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (n = 97; m <= 102; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
+
