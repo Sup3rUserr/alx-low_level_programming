7dc42e519c6bb73dbe6d587fdfd1abda13581c68
Yassin0xb98 <majidyassine0305@gmail.com>
2023-03-17 17:50:29 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,10 +5,11 @@
  */
 int main(void)
 {
-	char i;
-
-	for (i = 'z'; i >= 'a'; i--)
-		putchar(i);
-	putchar('\n');
-	return (0);
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
 }
