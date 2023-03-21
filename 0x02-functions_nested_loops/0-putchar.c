bcd4254cac52c21e6673b998a402a4487330a936
Yassin0xb98 <majidyassine0305@gmail.com>
2023-03-21 14:49:03 -0700

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,10 @@
-#iclude "main.h"
+#include "main.h"
 /**
  * *main-print_putchar
  * is written by me
  * Return:0
  */
-
-
-int main (void)
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -17,6 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
 return (0);
 }
