ec048f84f92763c88bb45c39bb003c882f243346
Yassin0xb98 <majidyassine0305@gmail.com>
2023-03-23 18:42:50 -0700

    0. Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h" 
+
+void positive_or_negative(int i)
+{
+		if (i > 0)
+	
+		puts(" is positive");
+	
+	else if (i == 0)
+	
+		puts(" is zero");
+	
+	else
+	
+		puts(" is negative");
+}
