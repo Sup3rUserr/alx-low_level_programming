9f629cdf9b51e103e21dba5389ba99e47048d28c
Yassin0xb98 <majidyassine0305@gmail.com>
2023-04-06 18:05:53 -0700

    Task07

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * str_len - Return length of string
+ * @s: The string
+ *
+ * Return: The length of the string
+ */
+
+int str_len(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + str_len(s + 1));
+	}
+}
+
+/**
+ * checkPal - Check if a string is a palindrome
+ * @s: The string to check
+ * @start: Starting index of string
+ * @end: Ending index of string
+ *
+ * Return: 1 if palindrome, 0 otherwise
+*/
+
+int checkPal(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] != s[end])
+	{
+		return (0);
+	}
+	return (checkPal(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: The string to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = str_len(s);
+
+	if (len <= 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (checkPal(s, 0, len - 1));
+	}
+}
